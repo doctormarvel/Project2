3c29db9ab560cbeba4b8ac9b1af3774900c14f5e
larrykridner <lkridner94@gmail.com>
2019-10-02 19:46:11 -0500

    finished

--- src/IDedLinkedList.java
+++ src/IDedLinkedList.java
@@ -14,11 +14,26 @@ public class IDedLinkedList <AnyType extends IDedObject>{
 	
 	public AnyType findID(int ID) {
 		AnyType x = null;
-		
+		Node last = head;
+		while(last != null) {
+			if(ID == last.data.getID()) {
+				x = last.data;
+			}
+			last = last.next;
+		}
+		return x;
 	}
 	
 	public boolean insertAtFront(AnyType x) {
-		
+		if (findID(x.getID()) == null) {
+			Node newNode = new Node(x);
+			newNode.next = head;
+			head = newNode;
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	public AnyType deleteFromFront(){
@@ -29,13 +44,31 @@ public class IDedLinkedList <AnyType extends IDedObject>{
 	}
 	
 	public AnyType delete(int ID){
-		
+			AnyType deleted = findID(ID);
+			Node temp = head;
+			if (deleted == null) {
+				return deleted;
+			}
+			else if (deleted.getID() == ID) {
+				head = temp.next;
+			}
+			else {
+				while(temp != null) {
+					if (deleted.getID() == temp.data.getID()) {
+						Node next = temp.next.next;
+						temp.next = next;
+					}
+				}
+			}
+			return deleted;
 	}
 	
 	public int printTotal(){
 		int total = 0;
-		while(head != null) {
-			total = total + head.data.getID();
+		Node last = head;
+		while(last != null) {
+			total = total + last.data.getID();
+			last = last.next;
 		}
 		return total;
 	}
